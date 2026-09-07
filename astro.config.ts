d0c59deaa855e869e33fb4a8a080abf5079caed5
Fuis18 <fuis18@proton.me>
2026-09-06 22:15:36 -0500

    Init Structure
    Delete tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fuis18.is-a.dev",
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+import type { Site, Links } from "@/types";
+
+export const SITE: Site = {
+  TITLE: "ぅいす",
+  DESCRIPTION:
+    ".",
+  AUTHOR: "Fuis18",
+};
+
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+  {
+    TEXT: "Blog",
+    HREF: "/blog",
+  },
+];
